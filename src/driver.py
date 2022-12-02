36a65c472dc76722d2234a413aff1f5590531e06
Kyrylo Maksymenko <saklar13@gmail.com>
2022-12-02 16:16:49 +0100

    move to Generic resource

--- src/driver.py
+++ src/driver.py
@@ -1,29 +1,16 @@
 from __future__ import annotations
 
-from logging import Logger
+from contextlib import contextmanager
 
 import jsonpickle
-from cloudshell.shell.core.driver_context import (
-    ApiVmDetails,
-    AutoLoadAttribute,
-    AutoLoadCommandContext,
-    AutoLoadDetails,
-)
+from cloudshell.api.cloudshell_api import InputNameValue, CloudShellAPISession
+from cloudshell.shell.core.driver_context import AutoLoadCommandContext, AutoLoadDetails
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 from cloudshell.shell.core.session.logging_session import LoggingSessionContext
 
-from data_model import GenericVPort, StaticVcenterVMConfig
 
-from cloudshell.cp.vcenter.actions.vm_network import VMNetworkActions
-from cloudshell.cp.vcenter.exceptions import VMIPNotFoundException
-from cloudshell.cp.vcenter.handlers.dc_handler import DcHandler
-from cloudshell.cp.vcenter.handlers.si_handler import SiHandler
-from cloudshell.cp.vcenter.handlers.vm_handler import VmHandler
-from cloudshell.cp.vcenter.resource_config import VCenterResourceConfig
-
-
-class GenericStaticVcenterVMShell2GDriver(ResourceDriverInterface):
+class GenericStaticVMShell2GDriver(ResourceDriverInterface):
     def initialize(self, context):
         pass
 
@@ -33,88 +20,57 @@ class GenericStaticVcenterVMShell2GDriver(ResourceDriverInterface):
     def get_inventory(self, context: AutoLoadCommandContext) -> AutoLoadDetails:
         with LoggingSessionContext(context) as logger:
             logger.info("Start Autoload process")
-            static_vm_conf = StaticVcenterVMConfig.create_from_context(context)
             api = CloudShellSessionContext(context).get_api()
-            vcenter_name = static_vm_conf.vcenter_resource_name
-            resource_conf = VCenterResourceConfig.from_cs_resource_details(
-                api.GetResourceDetails(vcenter_name), api=api
-            )
-            logger.info(
-                f"Credentials are {resource_conf.user} at {resource_conf.address}"
-            )
-            si = SiHandler.from_config(resource_conf, logger)
-            dc = DcHandler.get_dc(resource_conf.default_datacenter, si)
-            vm = dc.get_vm_by_path(static_vm_conf.vm_name)
+            cp_name = context.resource.attributes[
+                f"{context.resource.model}.Cloud Provider Resource Name"
+            ]
+            vm_path = context.resource.attributes[f"{context.resource.model}.VM Name"]
+            params = {
+                "vm_path": vm_path,
+                "model": context.resource.model,
+                "port_model": f"{context.resource.model}.GenericVPort",
+            }
+            command_inputs = [InputNameValue(k, v) for k, v in params.items()]
 
-            logger.info("Loading the IP of the VM")
-            vfw_ip = self._try_get_ip(dc, vm, resource_conf, logger)
-            if vfw_ip:
-                api.UpdateResourceAddress(context.resource.name, vfw_ip)
-            if not context.resource.address and not vfw_ip:
-                raise Exception(
-                    "Determination of the VM IP address failed."
-                    "Please, verify that the VM is up and running"
-                )
+            with run_with_sandbox(api) as rid:
+                json_data = api.ExecuteCommand(
+                    rid,
+                    cp_name,
+                    "Resource",
+                    "get_autoload_details_for_vm",
+                    command_inputs,
+                ).Output
 
-            self._add_ports(vm, static_vm_conf, logger)
+            autoload_details = jsonpickle.decode(json_data)
 
-            result = static_vm_conf.create_autoload_details("")
-            vm_api_res = api.GetResourceDetails(context.resource.name)
-            if not vm_api_res.VmDetails:
-                result.attributes.append(
-                    AutoLoadAttribute(
-                        "",
-                        "VmDetails",
-                        self._get_vm_details(vm.uuid, vcenter_name),
-                    )
-                )
-                logger.info("UUID updated")
-            elif vm_api_res.VmDetails.UID != vm.uuid:
-                raise Exception("Detected VM UUID doesn't match existing one.")
-            return result
+            info = api.GetResourceDetails(context.resource.name)
+            if info.VmDetails:
+                for attr in autoload_details.attributes[:]:
+                    if attr.attribute_name == "VmDetails":
+                        autoload_details.attributes.remove(attr)
+                        break
 
-    @staticmethod
-    def _try_get_ip(
-        dc: DcHandler,
-        vm: VmHandler,
-        resource_conf: VCenterResourceConfig,
-        logger: Logger,
-    ) -> str | None:
-        default_net = dc.get_network(resource_conf.holding_network)
-        try:
-            ip = VMNetworkActions(resource_conf, logger).get_vm_ip(
-                vm._entity,
-                default_net._entity,
-            )
-        except VMIPNotFoundException:
-            ip = None
-        return ip
-
-    @staticmethod
-    def _add_ports(
-        vm: VmHandler, static_vm_conf: StaticVcenterVMConfig, logger: Logger
-    ):
-        for vnic in vm.vnics:
-            net_number = vnic.label.lower().strip("network adapter ")
-            relative_address = f"P{net_number}"
-            port = GenericVPort(f"Port{net_number}")
-            port.mac_address = vnic.mac_address
-            port.requested_vnic_name = net_number
-            static_vm_conf.add_sub_resource(relative_address, port)
-            logger.info(
-                f"Added Port: model: {port.cloudshell_model_name} "
-                f"name: {port.name}, address: {relative_address}"
-            )
-
-    @staticmethod
-    def _get_vm_details(uuid: str, vcenter_name: str) -> str:
-        vm_details = ApiVmDetails()
-        vm_details.UID = uuid
-        vm_details.CloudProviderName = vcenter_name
-        vm_details.CloudProviderFullName = vcenter_name
-        vm_details.VmCustomParams = []
-        str_vm_details = jsonpickle.encode(vm_details, unpicklable=False)
-        return str_vm_details
+        return autoload_details
 
     def cleanup(self):
         pass
+
+
+@contextmanager
+def run_with_sandbox(api: CloudShellAPISession) -> str:
+    username = api.GetAllUsersDetails().Users[0].Name
+    r_name = "Quali CloudProvider Autodiscovery"
+    reservations = api.GetCurrentReservations(username).Reservations
+    for r in reservations:
+        if r.Name == r_name:
+            rid = r.Id
+            existed_reservation = True
+            break
+    else:
+        rid = api.CreateImmediateReservation(r_name, username, 120).Reservation.Id
+        existed_reservation = False
+    try:
+        yield rid
+    finally:
+        if not existed_reservation:
+            api.EndReservation(rid)
